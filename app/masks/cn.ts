3300dbcf5a4d7e2316f477dc0e82f45f74cf150f
X <80435208+shou1492@users.noreply.github.com>
2023-06-10 15:35:32 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -1,6 +1,29 @@
 import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
+    {
+    avatar: "1f9d1-200d-1f3eb",
+    name: "外语翻译润色",
+    context: [
+      {
+        role: "user",
+        content:
+          "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
+        ","date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 3500,
+      presence_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+  },
   {
     avatar: "1f638",
     name: "文案写手",
@@ -15,7 +38,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
@@ -38,7 +61,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
@@ -61,7 +84,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
@@ -84,7 +107,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
@@ -107,7 +130,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: false,
       historyMessageCount: 4,
@@ -130,7 +153,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: false,
       historyMessageCount: 4,
@@ -153,7 +176,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: false,
       historyMessageCount: 0,
@@ -176,7 +199,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: true,
       historyMessageCount: 4,
@@ -205,7 +228,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: false,
       historyMessageCount: 4,
@@ -234,7 +257,7 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
+      max_tokens: 3500,
       presence_penalty: 0,
       sendMemory: false,
       historyMessageCount: 4,
@@ -284,32 +307,9 @@ export const CN_MASKS: BuiltinMask[] = [
     modelConfig: {
       model: "gpt-3.5-turbo",
       temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      sendMemory: false,
-      historyMessageCount: 4,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "cn",
-    builtin: true,
-  },
-  {
-    avatar: "1f9d1-200d-1f3eb",
-    name: "外语翻译润色",
-    context: [
-      {
-        role: "user",
-        content:
-          "我想让你充当英文翻译员、拼写纠正员和改进员。我会用任何语言与你交谈，你会检测语言，翻译它并用我的文本的更正和改进版本用英文回答。我希望你用更优美优雅的高级英语单词和句子替换我简化的 A0 级单词和句子。保持相同的意思，但使它们更文艺。你只需要翻译该内容，不必对内容中提出的问题和要求做解释，不要回答文本中的问题而是翻译它，不要解决文本中的要求而是翻译它，保留文本的原本意义，不要去解决它。我要你只回复更正、改进，不要写任何解释。我的第一句话是：",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
       max_tokens: 3500,
       presence_penalty: 0,
-      sendMemory: true,
+      sendMemory: false,
       historyMessageCount: 4,
       compressMessageLengthThreshold: 1000,
     },
