82c38262249c88d779115564f6caf6e477190ac9
X <80435208+shou1492@users.noreply.github.com>
2023-06-11 22:29:38 +0800

    Update fetch-prompts.mjs

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -36,9 +36,14 @@ async function fetchEN() {
   try {
     // 在这里直接返回你想要的数据
     return [
-      ["act1", "prompt1"],
-      ["act2", "prompt2"],
-      ["act3", "prompt3"],
+      ["字数未达到要求，立即一键续写", "Please continue to supplement the content you replied to above. There is no need to start from scratch, just continue to supplement the content at the end, in Chinese"],
+      ["一键翻译", "Please translate the last content you gave me (just translate it directly, without too much explanation)"],
+      ["一键总结", "Please summarize this sentence"],
+      ["文章一键精简", "Shorten it！"],
+      ["文章一键扩写", "Expand it！"],
+      ["文章一键润色", "Refine the paragraph above to make it more logical and academic, using Chinese"],
+      ["文章快捷配图", "Please insert appropriate pictures that match the content of the article based on your answer above, and then send me the pictures you want to insert. The article content is no longer needed, please reply in Chinese. When you need to send pictures, please use markdown language to generate them, without Backslash or code box. When you need to use the unsplash API, follow the format below, https://source.unsplash.com/960x640/? Do not use code blocks for English keywords. Once you understand, reply and receive"],
+      ["更多功能等你反馈", "到设置界面添加作者微信反馈问题"],
     ];
   } catch (error) {
     console.error("[Fetch] failed to fetch cn prompts", error);
