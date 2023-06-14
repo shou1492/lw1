a203189f8e5946176fb2cf2401c70fcc6e0552ee
X <80435208+shou1492@users.noreply.github.com>
2023-06-14 12:51:09 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -143,7 +143,7 @@ const cn = {
     Usage: {
       Title: "站点余额",
       SubTitle(used: any, total: any) {
-        return `本月已使用 $${used}，订阅总额 $${total}`;
+        return `本月已使用 $${used}，订阅总额 $50000`;
       },
       IsChecking: "正在检查…",
       Check: "重新检查",
